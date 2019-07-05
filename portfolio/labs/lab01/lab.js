34296e30f44db1d0691f5c6f6b67c8fcbc017a7a
hankchen6602 <52521727+hankchen6602@users.noreply.github.com>
2019-07-05 17:59:29 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,9 +4,41 @@
 
 
 function start(e) {
+    var name=[
+        {
+            char :'陳',
+            big5 :'B3AF',
+            cns :'1-5D73',
+            unicode:'9673'
+        },
+        {
+            char :'昶',
+            big5 :'CEEB',
+            cns :'2-2A6C',
+            unicode:'6636'
+        },
+        {
+            char :'宇',
+            big5 :'A674',
+            cns :'1-4775',
+            unicode:'5B87'
+        }
+    ];
+
+    var ele = document.getElementById('div1');
+
+    for(var i= 0 ; i < 3 ; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 
 }
-
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 
